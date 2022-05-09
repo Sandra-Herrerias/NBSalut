91f0c3b2ca717f904eacd7a99eb7d65928a09893
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-05-09 11:22:41 +0200

    bootstrap installed

--- NBSalut_project/routes/web.php
+++ NBSalut_project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
