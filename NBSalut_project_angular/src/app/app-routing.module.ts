c1e28320dadd2c362bd7dc8b47c3b28656e6a3e3
David Rodriguez <davidrodriguez1470@gmail.com>
2022-05-12 10:30:14 +0200

    Add Angular Frontend source

---
+++ NBSalut_project_angular/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {//default
+    path: '**',
+    redirectTo: 'login'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ NBSalut_project_angular/src/app/components/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommunicatorService } from 'src/app/services/communicator.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  message: string | undefined;
+
+  public loginForm = this.formBuilder.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+      ]
+    ]
+  });
+
+
+  constructor(private formBuilder: FormBuilder, private communicatorService: CommunicatorService, private route: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(): void {
+    console.log({
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    })
+    this.communicatorService.login({
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    }).subscribe((res: any) => {
+      if (res.success) {
+        this.route.navigate(['/home']);
+      } else {
+        this.message = "Credenciales incorrectas.";
+      }
+    })
+  }
+
+}
