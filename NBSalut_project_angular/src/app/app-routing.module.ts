1429ef91c0589ec2064c87bd0a109abc09ea77ef
David Rodriguez <davidrodriguez1470@gmail.com>
2022-05-12 11:19:31 +0200

    Update registerVisit

--- NBSalut_project_angular/src/app/app-routing.module.ts
+++ NBSalut_project_angular/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterVisitComponent } from './components/visit/register-visit/register-visit.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'regvisit',
+    component: RegisterVisitComponent
   },
   {//default
     path: '**',
