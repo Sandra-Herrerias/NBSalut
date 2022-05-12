c959b568a9835645af289eeba0b0c8b5538e1ec0
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-05-12 14:40:00 +0200

    jordi userController code

--- NBSalut_project/app/Http/Controllers/UserController.php
+++ NBSalut_project/app/Http/Controllers/UserController.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers;
 use App\Models\User; 
+use Illuminate\Support\Facades\Auth; 
+use Validator;
+use Illuminate\Support\Facades\Hash;
+
 
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function login(Request $request){ 
-        $user = User::where('email', $request->_email)->first();
-        if(!$user || !Hash::check($request->_password, $user->password)){
+        $user = User::where('email', $request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
             return response()->json(['success' =>false ]);
         }
-        else{
-            $user=   Auth::loginUsingId($user->id);
+// return $user;
+            // $user= Auth::loginUsingId($user->id);
             return response()->json(['success' =>true, 'user' => $user ]);
-        }
 
     }
 }
