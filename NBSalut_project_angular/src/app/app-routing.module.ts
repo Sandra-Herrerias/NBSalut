c7c5dac21c3f4e198bbbd25b2afb5b699bb30cbc
David Rodriguez <davidrodriguez1470@gmail.com>
2022-05-12 12:21:34 +0200

    Update app-routing.module.ts

--- NBSalut_project_angular/src/app/app-routing.module.ts
+++ NBSalut_project_angular/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterVisitComponent } from './components/visit/register-visit/register-visit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'regvisit',
+    component: RegisterVisitComponent
+  },
+  {//default
+    path: '**',
+    redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
