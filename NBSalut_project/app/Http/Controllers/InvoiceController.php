5da16394c766d907b48f8f0c98d7f0f0cb42da9a
jordi <jordirocharocha@gmail.com>
2022-05-18 09:29:28 +0200

    changes

--- NBSalut_project/app/Http/Controllers/InvoiceController.php
+++ NBSalut_project/app/Http/Controllers/InvoiceController.php
@@ -3,22 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Models\Invoice;
 use DB;
 
 class InvoiceController extends Controller
 {
     public function getInvoices()
     {
-        $visits = DB::table('visits')
-            ->join('users', 'visits.id', '=', 'users.id')
-            ->join('uses', 'visits.id', '=', 'uses.visit_id')
-            ->join('offers', 'uses.treatment_id', '=', 'offers.treatment_id')
-            ->join('treatments', 'offers.treatment_id', '=', 'treatments.id')
-            ->join('invoices', 'visits.id', '=', 'invoices.visit_id')
-            ->select('visits.visit_date', 'invoices.id as invoice_id','users.dni', 'users.first_name', 'users.last_name', 'users.num_clinical_log', 'users.address', 'users.postal_code', 'treatments.name', 'treatments.price')
+            // ->select('visits.visit_date', 'invoices.id as invoice_id','users.dni', 'users.first_name', 'users.last_name', 'users.num_clinical_log', 'users.address', 'users.postal_code', 'treatments.name', 'treatments.price')
+
+            $invoices = DB::table('invoices')
+            ->join('visits', 'invoices.id', '=', 'visits.id')
+            ->join('partner_invoices', 'invoices.id', '=', 'partner_invoices.invoice_id')
+            ->join('invoice_details', 'invoices.id', '=', 'invoice_details.invoice_id')
+            ->join('treatments','invoice_details.treatment_id', '=','treatments.id')
+            ->where('partner_invoices.role', 'patient')
+            ->select()
             ->get();
 
+            if ($invoices) {
+                return response()->json(['success' => true, 'data' => $invoices]);
+            }
 
-        return response()->json(['success' => true, 'data' => $visits]);
+            return response()->json(['success' => false, 'data' => []]);
     }
 }
